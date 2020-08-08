0f126db2016665b0beffa89face1a56c5d8de5b2
alqadiano96 <alqadianoKira@gmail.com>
2020-08-07 17:32:12 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá, Mundo!</h1>
+      <h2>Start learning</h2>
+    </div>
+  );
+}
